90f47e258ae750eb7b194f28b7cd6e9efc47306a
CG-rgb777 <60811765+CG-rgb777@users.noreply.github.com>
2024-11-23 13:28:41 +0300

    Update AD.c

--- AD.c
+++ AD.c
@@ -1,5 +1,13 @@
-#include <windows.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+#include <process.h>
+
+
+
+int started_time_check = 0;
+
 
 int check_is_debugger_present() {
     return IsDebuggerPresent() ? 1 : 0;
@@ -45,8 +53,6 @@ int check_execution_time() {
 
 
 
-
-
 int CheckIfModuleLoaded_1() {
     const char *moduleName = "vehdebug-x86_64.dll";
     HMODULE hModule = GetModuleHandleA(moduleName);
@@ -69,7 +75,55 @@ int CheckIfModuleLoaded_2() {
 
 
 
+unsigned int __stdcall check_time(void *arg) {
+    time_t user_time = time(NULL);
+    if (user_time == (time_t)(-1)) {
+        perror("Ошибка при получении времени");
+        return 1;
+    }
+
+    time_t program_time = user_time;
+
+    while (1) {
+        Sleep(1000);
+        program_time++;
+
+        time_t current_time = time(NULL);
+        if (current_time == (time_t)(-1)) {
+            return 1;
+        }
+
+        if (abs((int)(current_time - program_time)) > 2) {
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return 0;
+}
+
+
+void start_time_check() {
+    HANDLE thread_handle;
+    unsigned thread_id;
+
+    thread_handle = (HANDLE)_beginthreadex(NULL, 0, check_time, NULL, 0, &thread_id);
+    if (thread_handle == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    CloseHandle(thread_handle);
+    int started_time_check = 1;
+}
+
+
+
+
+
 int detect_debugger() {
+    if(started_time_check == 0) {
+        start_time_check();
+    }
+    
     if (check_is_debugger_present() ||
         check_remote_debugger() ||
         check_hardware_breakpoints() ||
